deeb30f89c4d72daf354aabe46c397df48cd968d
Luis-Fernando-Hernandez-Reyes <116451159+luis28fer-hr@users.noreply.github.com>
2022-11-02 19:55:48 -0600

    Vista Inicio
    Se añadio el currusel de la vista inicio

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -3,8 +3,5 @@
 @section('titulo', 'Inicio')
 
 @section('contenido')
-    <h1>Home</h1>
-    <hr>
-    <p>Lorem ipsum dolor sit consectetur adipisicing elit. Corporis illo saepe fugiat recusandae necessitatibus harum labore quis eum laudantium esse ipsam eveniet, magnam excepturi ut omnis odit voluptates odio quam.</p>
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis labore sequi esse nihil nemo pariatur excepturi culpa. Sint illum obcaecati aspernatur facilis ullam cum quia laboriosam, deserunt corporis, veniam magnam!</p>
+    @include('parciales.menu.carrusel')
 @endsection
---
+++ resources/views/parciales/menu/carrusel.blade.php
@@ -0,0 +1,30 @@
+<section id="contendor_carrusel">
+    <div class="carrusel_item" id="item__1">
+        <div class="carrusel__tarjeta" id="tarjeta1__1">
+            <img src="https://i.pinimg.com/originals/8b/ac/6f/8bac6f1532fa65bf1a6291b49596abee.jpg" alt="Marvel Comics">
+        </div>
+        <div class="carrusel__flechas">
+            <a href="#item__3"><i class="fa-solid fa-circle-chevron-left"></i></a>
+            <a href="#item__2"><i class="fa-solid fa-circle-chevron-right"></i></a>
+        </div>
+    </div>
+    <div class="carrusel_item" id="item__2">
+        <div class="carrusel__tarjeta" id="tarjeta__2">
+            <img src="https://wallpapers-hub.art/wallpaper-images/328957.jpg" alt="Marvel Comics">
+        </div>
+        <div class="carrusel__flechas">
+            <a href="#item__1"><i class="fa-solid fa-circle-chevron-left"></i></a>
+            <a href="#item__3"><i class="fa-solid fa-circle-chevron-right"></i></a>
+        </div>
+    </div>
+    <div class="carrusel_item" id="item__3">
+        <div class="carrusel__tarjeta" id="tarjeta__3">
+            <img src="https://p4.wallpaperbetter.com/wallpaper/669/274/496/iron-man-angel-marvel-comics-marvel-comics-superhero-wallpaper-preview.jpg" alt="Marvel Comics">
+        </div>
+        <div class="carrusel__flechas">
+            <a href="#item__2"><i class="fa-solid fa-circle-chevron-left"></i></a>
+            <a href="#item__1"><i class="fa-solid fa-circle-chevron-right"></i></a>
+
+        </div>
+    </div>
+</section>
