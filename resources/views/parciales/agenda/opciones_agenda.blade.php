d3e68f9475c68c16ba683f4f25069bae465f6591
Luis-Fernando-Hernandez-Reyes <116451159+luis28fer-hr@users.noreply.github.com>
2022-11-05 17:08:58 -0600

    Se agrego las opciones de la vista agenda

---
+++ resources/views/parciales/agenda/opciones_agenda.blade.php
@@ -0,0 +1,23 @@
+<section id="caja-opciones">
+    <div class="opciones" id="opciones-1">
+        <p>Marcas</p>
+        <div>
+            <ul>
+                <li><a class="" id="nuevo" href=""><i class="fa-sharp fa-solid fa-square-plus"></i><button>Nuevo</button></a></li>
+                <li><a class="" id="consultar" href=""><i class="fa-sharp fa-solid fa-magnifying-glass-plus"></i><button>Consultar</button></a></li>
+                <li><a id="reporte" href="#"><i class="fa-solid fa-file-pdf"></i><button>Reporte</button></a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="opciones" id="opciones-2">
+        <p>Proovedores</p>
+        <div>
+            <ul>
+                <li><a class="" id="nuevo" href=""><i class="fa-sharp fa-solid fa-square-plus"></i><button>Nuevo</button></a></li>
+                <li><a class="" id="consultar" href=""><i class="fa-sharp fa-solid fa-magnifying-glass-plus"></i><button>Consultar</button></a></li>
+                <li><a id="reporte" href="#"><i class="fa-solid fa-file-pdf"></i><button>Reporte</button></a></li>
+            </ul>
+        </div>
+    </div>
+
+</section>
