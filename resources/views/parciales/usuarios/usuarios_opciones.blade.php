d6bb0f081319d49fc048c84da04d8f8e3830fecf
Luis-Fernando-Hernandez-Reyes <116451159+luis28fer-hr@users.noreply.github.com>
2022-11-09 22:05:10 -0600

    Ejemplo de ruta y funcion para editar

--- resources/views/parciales/usuarios/usuarios_opciones.blade.php
+++ resources/views/parciales/usuarios/usuarios_opciones.blade.php
@@ -5,7 +5,7 @@
             <div>
                 <ul>
                     <li><a class="{{request()->routeIs('UsuariosAgregar')? 'agregar_Activa':''}}" id="nuevo" href="{{route('UsuariosAgregar')}}"><i class="fa-sharp fa-solid fa-square-plus"></i><button>Nuevo</button></a></li>
-                    <li><a class="{{request()->routeIs('UsuariosConsultar')? 'consultar_Activa':''}}" id="consultar" href="{{route('UsuariosConsultar')}}"><i class="fa-sharp fa-solid fa-magnifying-glass-plus"></i><button>Consultar</button></a></li>
+                    <li><a class="{{request()->routeIs('UsuariosConsultar*')? 'consultar_Activa':''}}" id="consultar" href="{{route('UsuariosConsultar')}}"><i class="fa-sharp fa-solid fa-magnifying-glass-plus"></i><button>Consultar</button></a></li>
                     <li><a id="reporte" href="#"><i class="fa-solid fa-file-pdf"></i><button>Reporte</button></a></li>
                 </ul>
                 <form action="#">
